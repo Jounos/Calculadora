2948f360a23007ba35a25ac333125e71587a7136
Geovanny Neves Liberato <gnliberato@outlook.com>
2019-09-03 22:21:14 -0300

    Implementado teste unitário do service CalculadoraService

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,12 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
 describe('CalculadoraService', () => {
+  const service = new CalculadoraService;
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: CalculadoraService = TestBed.get(CalculadoraService);
     expect(service).toBeTruthy();
   });
+
+  it('deve garantir que 1 + 4 = 5', () => {
+    const soma = service.calcular(1, 4, CalculadoraService.SOMA);
+    expect(soma).toEqual(5);
+  });
+
+  it('deve garantir que 1 - 4 = -3', () => {
+    const subtracao = service.calcular(1, 4, CalcularService.SUBTRACAO);
+    expect(subtracao).toEqual(-3);
+  });
+
+  it('deve garantir que 1 / 4 = 0.25', () => {
+    const divisao = service.calcular(1, 4, CalculadoraService.DIVISAO);
+    expect(divisao).toEqual(0.25);
+  });
+  
+  it('deve garantir que 1 * 4 = 4', () => {
+      let multiplicacao = service.calcular(1, 4, CalculadoraService.MULTIPLICACAO);
+      expect(multiplicacao).toEqual(4);
+    });
+
+  it('deve retornar 0 para operação inválida', () => {
+      let operacaoInvalida = service.calcular(1, 4, '%');
+      expect(operacaoInvalida).toEqual(0);
+    });
 });
